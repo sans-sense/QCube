d67b3320e532a22588e07f28758d63ce0eed4c73
sans-sense <apurba.n@Imaginea.com>
2012-09-13 19:37:06 +0530

    moved ml-demo to new versions using common resources

--- demo/common/demo.ui.js
+++ demo/common/demo.ui.js
@@ -11,7 +11,7 @@ $(function() {
         }
 
         function drawModel() {
-            var model  = new PivotTableModel(dimensions, 'Commit', de.tree);
+            var model  = new PivotTableModel(dimensions, de.measure, de.tree);
             model.compute();
             de['model'] = model;
             new PivotRenderer('table', model).draw();
@@ -46,55 +46,4 @@ $(function() {
             this.ondragover = doDragOver;
         });
     }()); // end of addDrag
-
-    (function addToggleBehavior() {
-        var targetBtn = 'button#toggle-data';
-        $(targetBtn).click(function(){ 
-            var btnHtml;
-            $('div.data').toggle(); 
-            btnHtml =  (($('div.data:visible').length > 0)? 'Hide':'Show') + ' Data';
-            $(targetBtn).html(btnHtml);
-        });
-    }()); // end of add toggle
-
-    $('button#compute-tree').click(function(){         
-        var domainInstances = createDomainInstances();
-        demoExports.computeQCTree(domainInstances);
-    });
-    
-    de['eventManager'].on('cube-computed', function(treeData) {
-        var avgRating = treeData["1:{Rating}"];
-        $('#cube_summary').html("Average rating of movies analyzed is "+ avgRating);
-        $('table').find("tbody tr").remove();
-        $('table').find('tbody')
-            .append($('<tr>')
-                    .append($('<td>').text(avgRating))
-                   );
-    });
-
-    // read the inputs and create the domain instances
-    function createDomainInstances(){
-        var domainInstances = {};
-        $('div.data textarea').each(function() {
-            var elementId = this.id, elementType, values, currentDomainInstance;
-            elementType = elementId.substring(9);
-            elementType = elementType[0].toUpperCase() + elementType.substring(1);
-            domainInstances[elementType] = [];
-            values = this.value.split('\n');
-            _.each(values, function(value, index) {
-                if (value.trim().length > 0) {
-                    currentDomainInstance = demoExports['dm'][elementType].create(value.trim());
-                    if (currentDomainInstance.id) {
-                        domainInstances[elementType][currentDomainInstance.id] = currentDomainInstance
-                    } else {
-                        domainInstances[elementType].push(currentDomainInstance);
-                    }
-                }
-            });
-        });
-        demoExports['di'] = domainInstances;
-        return domainInstances;
-    }
-
-    $('button#compute-tree').click();
 });
