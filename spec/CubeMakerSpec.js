45a4dc76e5706da3e717f3aeac3b1cf0f4761299
sans-sense <apurba.n@Imaginea.com>
2013-03-12 20:48:49 +0530

    started on tree, handles ub n.e. lb scenario

--- spec/CubeMakerSpec.js
+++ spec/CubeMakerSpec.js
@@ -11,16 +11,82 @@ describe("CubeMaker", function() {
             ['S1', 'P2', 's', 12],
             ['S2', 'P1', 'f', 9]
         ];
+        this.cmInternals = {};
+        CB.exportTo(this.cmInternals);
     });
     
-    it('UpperBound supports jumps, so from the cell c = (a2; *; *; *), we can go to(a2; b1; *; d1)', function(){
-        var stats = [], upperBound;
-        stats[1] = { order_keys : ['b1']};
-        stats[2] = { order_keys : []};
-        stats[3] = { order_keys : ['d1']};
+    it('UpperBound supports jumps, so from the cell c = (s1; *; *), we can go to(s1; *; s) in case there is only one value s for S1', function(){
+        var  upperBound, statsWrapper;
 
-        upperBound = CB.computeUpperBound(stats, ['a2', '*','*','*'], 4);
-        expect(upperBound).toEqual(['a2', 'b1','*','d1']);
+        statsWrapper = createTestStats([null, ['P1', 'P2'], 's']);
+        upperBound = this.cmInternals.computeUpperBound(statsWrapper, ['s1', '*','*'], 3);
+
+        expect(upperBound).toEqual(['s1', '*','s']);
     });
+
+    it('tempclasses are generated for all dimension combinations existing in the data', function(){
+        var tempClasses;
+
+        this.cmInternals.configure(this.tableData);
+        tempClasses = this.cmInternals.createTempClasses();
+        console.log(JSON.stringify(tempClasses));
+        expect(tempClasses.length).toEqual(11);
+    });
+
+    it('tempclasses are sorted in dictionary order, * rules', function() {
+        var unsortedBounds = [["-1","*","*"], ["*","*","*"], ["*","P","*"]], sortedClasses;
+        sortedClasses = this.cmInternals.sortTempClasses(createNestedArray(unsortedBounds, 1));
+        expect(sortedClasses.length).toEqual(3);
+        expect(sortedClasses[0][1]).toEqual(unsortedBounds[1]);
+        expect(sortedClasses[2][1]).toEqual(unsortedBounds[0]);
+    });
+
+    it('tree contains nodes for all tempclasses', function(){
+        var upperBounds, qcTree;
+        upperBounds = [['*', '*', '*'], ['*', 'p1', '*'], ['S1', '*', 's']];
+        qcTree  = this.cmInternals.createQCTree(createNestedArray(upperBounds, 1));
+        expect(qcTree.root.label).toEqual('root');
+        console.log(JSON.stringify(qcTree));
+    });
+
+    /**
+      * Creates test stats about dimensional values. Each value passed in the dimension is added
+      * as an entry against order_key field in the stat for that dimension. For null values creates
+      * an empty stat entry. e.g if values is [null, ['P1', 'P2'], 's'], will create
+      *  {"stats":[{"order_keys":[]},{"order_keys":["P1", "P2"]},{"order_keys":['s']}]}
+      */
+    function createTestStats(values) {
+        var stats, i, value, statsWrapper, statValue;
+        stats = [];
+        for ( i = 0; i < values.length; i++) {
+            value = values[i];
+            statValue = [];
+            if (value) {
+                if (value instanceof Array) {
+                    statValue.push.apply(statValue, value);
+                } else {
+                    statValue.push(value);
+                }
+            }
+            stats.push({ order_keys: statValue});
+        }
+        statsWrapper = {
+            stats : stats,
+            get: function(index) {
+                return stats[index];
+            }
+        };
+        return statsWrapper;
+    }
+
+    function createNestedArray(values, valuePosition) {
+        var results = [], i, j, row1;
+        for ( i = 0; i < values.length; i++) {
+            row1 = [];
+            row1[valuePosition] = values[i];
+            results.push(row1);
+        }
+        return results;
+    }
 });
     
--- src/cubeMaker.js
+++ src/cubeMaker.js
@@ -1,34 +1,63 @@
+
 var CB = {};
 
 (function(){
+    "use strict";
+
     var tableData, factIndex, tempClasses, star, dimCount;
 
     star = '*';
-
-    this.createCube = function(tableData, factIndex) {
-        var rootNode;
-        tableData = tableData;
-        factIndex = factIndex;
-
-        dimCount = tableData[0].length - 1;
-
-        // this is the all star node
-        rootNode = createRootNode(dimCount);
-
-        tempClasses = [];
-
-        //start with dfs on rootNode
-        dfs(rootNode, createNaturalNumberArray(tableData.length), 0);
-
-        // merge tempclasses
-
-        // remove lower bounds that descendents in merged classes
-
-        // TODO return what was really meant
-        return tempClasses;
+    
+    this.createCube = function(tableData) {
+        var qcTree;
+        configure(tableData);
+        tempClasses = createTempClasses(dimCount, tableData, factIndex);
+        tempClasses = sortTempClasses(tempClasses);
+        qcTree = createQCTree(tempClasses);
+        return qcTree;
     };
 
-    function createRootNode(wildCardCount) {
+    function configure(pTableData) {
+        tableData = pTableData;
+        dimCount = tableData[0].length - 1;
+        factIndex = dimCount;
+    }
+
+    function createTempClasses() {
+        var allStarNode;
+        tempClasses = [];
+
+        allStarNode = createAllStarNode(dimCount);
+        dfs(allStarNode, createNaturalNumberArray(tableData.length), 0);
+
+        return tempClasses;
+    }
+
+    function sortTempClasses(tempClasses) {
+        return tempClasses.sort(function(clazz1, clazz2) {
+            return (clazz1[1] < clazz2[1])? -1:1;
+        });
+    }
+
+    function createQCTree(tempClasses) {
+        var qcTree, lastUB, currentUB, i;
+        qcTree = {};
+        qcTree.root = new QNode('root', -1, tempClasses[0][4]);
+        lastUB = tempClasses[0][1];
+
+        for ( i = 1; i < tempClasses.length; i++) {
+            currentUB = tempClasses[i][1];
+            if (isEqual(currentUB, lastUB)) {
+                //TODO create edge between current ub and last UB for the dim on which they differ
+            } else {
+                insertNodes(qcTree, currentUB, tempClasses[i][4]);
+                lastUB = currentUB;
+            }
+        }
+        return qcTree;
+    }
+
+    function createAllStarNode(wildCardCount) {
         var node = [], i;
         for (i = 0; i < wildCardCount; i++) {
             node.push(star);
@@ -36,30 +65,110 @@ var CB = {};
         return node;
     }
 
-    function dfs(node, partition, dimNumber, pId) {
-        var aggregate, stats;
-        stats = new CompositeOrderedHash(dimCount);
+    function dfs(lowerBound, dataIndices, dimNumber, pId) {
+        var aggregate, stats, classId, upperBound, tempClass;
 
-        // we use stats while computing the upper bound jumps
-        aggregate = collectStatsAndAdd(computeSum, partition, stats);
+        // if there are no elements in the partition, we can skip it
+        if (dataIndices.length === 0) {
+            return;
+        }
+        stats = new CompositeStat(dimCount);
+        classId = tempClasses.length;
+
+        aggregate = collectStatsAndAdd(computeSum, dataIndices, stats);
+        upperBound = computeUpperBound(stats, lowerBound, dimCount);
+
+        // we do not handle multiple measures yet
+        tempClass = [classId, upperBound, lowerBound, dimNumber, aggregate];
+        tempClasses.push(tempClass);
         
-        upperBound = computeUpperBound(stats, node, dimCount);
+        if (redundantCompute(dimNumber, lowerBound, upperBound)) {
+            return;
+        }
+
+        processChildren(dimNumber, upperBound, stats);
     }
 
+
     // TODO this should handle any operation, meaning we need to get the initial value and then incrementatlly operate, something like iteratee
-    function collectStatsAndAdd(operation, partition, dimStats) {
-        var i, total, value, row, j, dimValue, dimStat;
+    // TODO: performance: currently this is run for every partition, this means for the same n records it works again and again
+    function collectStatsAndAdd(aggOperation, partition, dimStats) {
+        var i, total, value, row, j, dimValue, dimStat, rowIndex;
         total = 0;
         for (i = 0; i < partition.length; i++) {
+            rowIndex = partition[i];
             row = tableData[rowIndex];
             value = row[factIndex];
-            total = operation.apply(this, [value, total]);
+            total = aggOperation.call(value, total);
             for (j = 0; j < dimCount; j++) {
                 dimValue = row[j];
                 dimStats.set(j, dimValue, i);
             }
         }
+        return total;
     }
+
+    function computeUpperBound(stats, lowerBound, dimCount) {
+        var upperBound, i, dimValue;
+        upperBound = cloneArray(lowerBound);
+
+        for (i = 0; i < dimCount; i++) {
+            dimValue = lowerBound[i];
+            
+            if(dimValue === star){
+                if(stats.get(i).order_keys.length === 1){
+                    upperBound[i] = stats.get(i).order_keys[0];
+                }
+            }else{
+                upperBound[i] = dimValue;
+            }
+        }
+        return upperBound; 
+    }
+
+    /**
+      * if lowerBound is (*,*,10) and upperBound is (*,F,10) and dimNumber 2, the compute is redundant
+      * as all combinations have already been computed.  Original data was [[80,f,10], [97, f,10]]
+      */
+    function redundantCompute(dimNumber, lowerBound, upperBound) {
+        var i;
+        for( i = 0; i < (dimNumber -1); i++){
+            if((lowerBound[i] === star ) && (upperBound[i] !== star)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function processChildren(dimNumber, upperBound, stats) {
+        var i, partitions, partition, j;
+
+        for ( i = dimNumber; i < dimCount; i++) {
+            if (upperBound[i] === star) {
+                partitions = createPartitions(stats, i, upperBound);
+                for ( j = 0; j < partitions.length; j++) {
+                    partition = partitions[j];
+                    dfs(partition.lowerBound, partition.dataIndices, partition.dimNumber);
+                }
+            }
+        }
+    }
+
+    function createPartitions(stats, dimNumber, parentUpperBound) {
+        var stat, keyLength, i, partitions, dimValue, lowerBound;
+        stat = stats.get(dimNumber);
+        keyLength = stat.order_keys.length;
+        partitions = [];
+
+        for (i = 0; i < keyLength; i++) {
+            dimValue = stat.order_keys[i];
+            lowerBound = cloneArray(parentUpperBound);
+            lowerBound[dimNumber] = dimValue;
+            partitions.push({lowerBound:lowerBound, dimNumber: dimNumber, dataIndices: stat.data[dimValue]});
+        }
+        return partitions;
+    }
+
 
     function computeSum(value, total) {
             return total + value;
@@ -70,28 +179,47 @@ var CB = {};
         for (i = 0; i < arrayLength; i++){
             naturalNumbers.push(i);
         }
+        return naturalNumbers;
     }
     
-    function computeUpperBound(stats, lowerBound, dimCount) {
-        var upperBound, i, dimValue;
-        upperBound = [];
-        upperBound.push.apply(upperBound, lowerBound);
-
-        for (i = 0; i < dimCount; i++) {
-            dimValue = lowerBound[i];
-            
-            if(dimValue === star){
-                if(stats[i].order_keys.length === 1){
-                    upperBound[i] = stats[i].order_keys[0];
+    function insertNodes(tree, upperBound, value) {
+        var i, dimValue, parentNode, childNode;
+        parentNode = tree.root;
+        // insert all the non * as nodes in the tree
+        for ( i = 0; i < upperBound.length; i++) {
+            dimValue = upperBound[i];
+            if (dimValue !== star) {
+                childNode = parentNode.findChild(i, dimValue);
+                if (childNode) {
+                    parentNode = childNode;
+                } else {
+                    childNode = new QNode(dimValue, i);
+                    if ( noStarAfterThis(upperBound, i)) {
+                        childNode.value = value;
+                        childNode.isLeaf = true;
+                    }
+                    parentNode.addChild(childNode);
+                    parentNode = childNode;
                 }
-            }else{
-                upperBound[i] = dimValue;
             }
         }
-        return upperBound; 
     }
 
-    function OrderedHash(){
+    function noStarAfterThis(valueArray, currentIndex) {
+        var i;
+        for ( i = currentIndex + 1; i < valueArray.length; i++) {
+            if (valueArray[i] !== star) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+      * Captures information about a dimension, details like which data correspond to 
+      * these value and what are the possible values allowed for this dimension
+      */
+    function DimensionStat(){
         this.data = {};
         this.order_keys = [];
     }
@@ -105,27 +233,89 @@ var CB = {};
         this.get = function(key) {
             return this.data[key];
         };
-    }).call(OrderedHash.prototype);
+    }).call(DimensionStat.prototype);
 
-    function CompositeOrderedHash() {
+    /**
+      * Placeholder for multiple DimensionStat
+      */
+    function CompositeStat() {
         var i;
         this.hashes = [];
         for( i = 0; i < dimCount; i++) {
-            this.hashes.push(new OrderedHash());
+            this.hashes.push(new DimensionStat());
         }
         this.get = function(index) {
             return this.hashes[index];
         };
         this.set = function(dimIndex, dimValue, rowIndex){
             var dimStat = this.hashes[dimIndex];
-            if (!dimStat.get(value)) {
-                dimStat.set(value, []);
+            if (!dimStat.get(dimValue)) {
+                dimStat.set(dimValue, []);
             }
             //TODO: performance: we are doing get twice, should avoid
-            dimStat.get(value).push(rowIndex);
+            dimStat.get(dimValue).push(rowIndex);
         };
     }
 
-    this.computeUpperBound = computeUpperBound;
+    /**
+      * A node that captures label, value and children
+      */
+    function QNode(label, dimNumber, value, isLeaf) {
+        this.label = label;
+        this.value = value;
+        this.children = [];
+        this.edges = [];
+        this.dimNumber = dimNumber;
+        this.isLeaf = (isLeaf)? true:false;
+    }
 
+    (function(){
+        this.addChild = function(childNode) {
+            this.children.push(childNode);
+        };
+        this.findChild = function(dimNumber, label) {
+            var i, child;
+            for ( i = 0; i < this.children.length; i++) {
+                child = this.children[i];
+                if (( child.dimNumber === dimNumber ) && (child.label === label)) {
+                    return child;
+                }
+            }
+            return null;
+        };
+    }).call(QNode.prototype);
+
+    /**
+      * Named Link to any other node.
+      */
+    function QLink(label, endNode) {
+        this.label = label;
+        this.endNode = endNode;
+    }
+
+    function cloneArray(original) {
+        var clonedArray = [];
+        clonedArray.push.apply(clonedArray, original);
+        return clonedArray;
+    }
+
+    function isEqual(a1, a2) {
+        return (a1.toString() === a2.toString());
+    }
+
+    /**
+      * Ideally we do not need to see the guts of the implementation, however for 
+      * testing, it makes sense to have a magic reference to the important 
+      * methods. So exportTo exports the internal methods out for testing. In actual
+      * production we should not need this at all.
+      */
+    function exportTo(nameSpaceHolder) {
+        nameSpaceHolder.computeUpperBound = computeUpperBound;
+        nameSpaceHolder.createTempClasses = createTempClasses;
+        nameSpaceHolder.configure = configure;
+        nameSpaceHolder.sortTempClasses = sortTempClasses;
+        nameSpaceHolder.createQCTree = createQCTree;
+    }
+
+    this.exportTo = exportTo;
 }).call(CB);
